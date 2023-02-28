bac994fcff11a7c7094a345ff5fd1c7ccd4adcdf
ulfiizza <ulfiizza3@gmail.com>
2023-02-28 20:39:28 +0700

    Praktikum 2 - Controller Web Framework Laravel

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return 'Selamat Datang';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function about()
+    {
+        return 'Ulfi Mustatiq Abidatul Izza, 2141720052';
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function articles($id)
+    {
+        return 'Halaman Artikel dengan Id ' . $id;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [PageController::class, 'articles']);
